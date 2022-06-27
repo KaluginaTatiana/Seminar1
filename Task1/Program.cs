0b08fd3459fda60e52b06d1deadf329e838244cc
Tatiana <ttt-ttt@bk.ru>
2022-06-27 21:41:29 +0300

    Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,7 @@
 ﻿// Напишите программу, которая на вход принимает число и на выход выдает его квадрат.
-double num = 5;
+Console.WriteLine("Введите число, которое нужно возвести в квадрат");
+double num = Convert.ToDouble(Console.ReadLine());
 double sqr = Math.Pow(num, 2);
-Console.WriteLine(sqr);
+// double sqr = num*num; - можно и так
+Console.WriteLine($"Квадрат числа {num} равен {sqr}");
+Console.WriteLine("Квадрат числа " + num + " равен " + sqr + ".");
