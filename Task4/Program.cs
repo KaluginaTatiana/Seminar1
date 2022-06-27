fae6361961c0fde1f0616420505e5a156852715f
Tatiana <ttt-ttt@bk.ru>
2022-06-27 23:41:32 +0300

    Task4_creating a method

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿// Создание метода для возведения в квадрат целого числа
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Metod(num);
+
+void Metod(int currentNumber)
+{
+    Console.WriteLine(currentNumber * currentNumber);
+}
